4b15c582ea6859ee8b6760250b7839052f17c30f
RichardLe0201 <45153869+RichardLe0201@users.noreply.github.com>
2020-08-05 14:02:50 -0700

    Teachers.tsx is broken (please look)

--- src/main/frontend/src/pages/Teachers.tsx
+++ src/main/frontend/src/pages/Teachers.tsx
@@ -1,69 +1,93 @@
-import React from 'react';
-import DashboardLayout from '../components/DashboardLayout';
-import {Table, Form, Button} from 'react-bootstrap'
+import { Calendar, Component, createElement, DayHeaderContentArg } from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 
-function Teachers(props: AuthenticatedComponentProps) {
-    return (
-      <DashboardLayout {...props} >
-        <Table striped bordered size="sm">
-          <thead>
-            <tr>
-              <th>Monday</th>
-              <th>Tuesday</th>
-              <th>Wednesday</th>
-              <th>Thursday</th>
-              <th>Friday</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Accepted:</td>
-              <td>Accepted:</td>
-              <td>Accepted:</td>
-              <td>Accepted:</td>
-              <td>Accepted:</td>
-            </tr>
-            <tr>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-            </tr>
-            <tr>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-            </tr>
-            <tr>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-            </tr>
-            <tr>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-            </tr>
-            <tr>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-              <td><Form.Group controlId="formBasicCheckbox"><Form.Check type="checkbox" label="Check me out" /></Form.Group></td>
-            </tr>
-          </tbody>
-        </Table>
-        <Button variant="success" size="sm" block style={{marginBottom: '1rem'}}>Accept</Button>
-        <Button variant="danger" size="sm" block>Reject</Button>
-      </DashboardLayout >
-    )
+export default function Students(props: AuthenticatedComponentProps){
+
+document.addEventListener('DOMContentLoaded', function() {
+  let calendarEl: HTMLElement = document.getElementById('calendar')!;
+
+  class CustomDayHeader extends Component<{ text: string }> {
+    render() {
+      return createElement('div', {}, '!' + this.props.text + '!')
+    }
   }
 
-  export default Teachers;
+  let calendar = new Calendar(calendarEl, {
+    plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin ],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    initialDate: '2018-01-12',
+    navLinks: true,
+    editable: true,
+    dayMaxEvents: true,
+    dayHeaderContent(arg: DayHeaderContentArg) {
+      return createElement(CustomDayHeader, { text: arg.text })
+    },
+    events: [
+      {
+        title: 'All Day Event',
+        start: '2018-01-01',
+      },
+      {
+        title: 'Long Event',
+        start: '2018-01-07',
+        end: '2018-01-10'
+      },
+      {
+        groupId: '999',
+        title: 'Repeating Event',
+        start: '2018-01-09T16:00:00'
+      },
+      {
+        groupId: '999',
+        title: 'Repeating Event',
+        start: '2018-01-16T16:00:00'
+      },
+      {
+        title: 'Conference',
+        start: '2018-01-11',
+        end: '2018-01-13'
+      },
+      {
+        title: 'Meeting',
+        start: '2018-01-12T10:30:00',
+        end: '2018-01-12T12:30:00'
+      },
+      {
+        title: 'Lunch',
+        start: '2018-01-12T12:00:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2018-01-12T14:30:00'
+      },
+      {
+        title: 'Happy Hour',
+        start: '2018-01-12T17:30:00'
+      },
+      {
+        title: 'Dinner',
+        start: '2018-01-12T20:00:00'
+      },
+      {
+        title: 'Birthday Party',
+        start: '2018-01-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2018-01-28'
+      }
+    ]
+  });
+
+  calendar.render();
+})
+
+};
