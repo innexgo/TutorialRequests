9ace83a1c352d0cc9b32146189053b07a3145838
RichardLe0201 <45153869+RichardLe0201@users.noreply.github.com>
2020-08-05 16:34:32 -0700

    Updates to Students & Teachers Calendars

--- src/main/frontend/src/pages/Students.tsx
+++ src/main/frontend/src/pages/Students.tsx
@@ -1,95 +1,29 @@
-import React from 'react';
+import React from 'react'
+import FullCalendar  from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 import DashboardLayout from '../components/DashboardLayout';
-import {Table, Dropdown, DropdownButton, Form, Button} from 'react-bootstrap'
 
-export default function Students(props: AuthenticatedComponentProps){
-
+function Students(props: AuthenticatedComponentProps) {
   return (
   <DashboardLayout {...props} >
-    <Table striped bordered size="sm">
-      <thead>
-        <tr>
-          <th>Monday</th>
-          <th>Tuesday</th>
-          <th>Wednesday</th>
-          <th>Thursday</th>
-          <th>Friday</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <DropdownButton title="Choose Teacher">
-              <Dropdown.Item as="button">Ms. Ng</Dropdown.Item>
-              <Dropdown.Item as="button">Ms. Cornejo</Dropdown.Item>
-              <Dropdown.Item as="button">Ms.Dimas</Dropdown.Item>
-            </DropdownButton>
-          </td>
-          <td>
-            <DropdownButton title="Choose Teacher">
-              <Dropdown.Item as="button">Ms. Ng</Dropdown.Item>
-              <Dropdown.Item as="button">Ms. Cornejo</Dropdown.Item>
-              <Dropdown.Item as="button">Ms.Dimas</Dropdown.Item>
-            </DropdownButton>
-          </td>
-          <td>
-            <DropdownButton title="Choose Teacher">
-              <Dropdown.Item as="button">Ms. Ng</Dropdown.Item>
-              <Dropdown.Item as="button">Ms. Cornejo</Dropdown.Item>
-              <Dropdown.Item as="button">Ms.Dimas</Dropdown.Item>
-            </DropdownButton>
-          </td>
-          <td>
-            <DropdownButton title="Choose Teacher">
-              <Dropdown.Item as="button">Ms. Ng</Dropdown.Item>
-              <Dropdown.Item as="button">Ms. Cornejo</Dropdown.Item>
-              <Dropdown.Item as="button">Ms.Dimas</Dropdown.Item>
-            </DropdownButton>
-          </td>
-          <td>
-            <DropdownButton title="Choose Teacher">
-              <Dropdown.Item as="button">Ms. Ng</Dropdown.Item>
-              <Dropdown.Item as="button">Ms. Cornejo</Dropdown.Item>
-              <Dropdown.Item as="button">Ms.Dimas</Dropdown.Item>
-            </DropdownButton>
-          </td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>
-            <Form>
-            <Form.Group controlId="checkbox">
-              <Form.Check type="checkbox" label="Mr. Taylor" />
-            </Form.Group>
-            </Form>
-          </td>
-          <td>
-            <Form>
-            <Form.Group controlId="checkbox">
-              <Form.Check type="checkbox" label="Jack Chen" />
-            </Form.Group>
-            </Form>
-          </td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>
-            <Form>
-            <Form.Group controlId="checkbox">
-              <Form.Check type="checkbox" label="Ms. Cole" />
-            </Form.Group>
-            </Form>
-          </td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </tbody>
-    </Table>
-    <Button className="ml-2">Request</Button>
-    <Button className="ml-2">Cancel</Button>
-    </DashboardLayout>
-  );
-}
+    <FullCalendar
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      headerToolbar={{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      }}
+      initialView='dayGridMonth'
+      editable={true}
+      selectable={true}
+      selectMirror={true}
+      dayMaxEvents={true}
+      weekends={false}
+      initialEvents={[]}
+    />
+  </DashboardLayout>
+)};
+
+export default Students;
--- src/main/frontend/src/pages/Teachers.tsx
+++ src/main/frontend/src/pages/Teachers.tsx
@@ -3,23 +3,27 @@ import FullCalendar  from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import DashboardLayout from '../components/DashboardLayout';
 
 function Teachers(props: AuthenticatedComponentProps) {
-  return <FullCalendar
-    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-    headerToolbar={{
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay'
-    }}
-    initialView='dayGridMonth'
-    editable={true}
-    selectable={true}
-    selectMirror={true}
-    dayMaxEvents={true}
-    weekends={false}
-    initialEvents={[]} 
-  />
-};
+  return (
+  <DashboardLayout {...props} >
+    <FullCalendar
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      headerToolbar={{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      }}
+      initialView='dayGridMonth'
+      editable={true}
+      selectable={true}
+      selectMirror={true}
+      dayMaxEvents={true}
+      weekends={false}
+      initialEvents={[]}
+    />
+  </DashboardLayout>
+)};
 
 export default Teachers;
