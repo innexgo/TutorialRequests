5f2b045ca4c21d78d0bafdf193999d630132d1c3
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2020-08-30 15:16:34 -0700

    Added SendMail function for email verif.

---
+++ src/main/java/innexgo/hours/SendMail.java
@@ -0,0 +1,52 @@
+package hours;
+
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.*;
+import com.sendgrid.helpers.mail.objects.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+
+public class SendMail {
+  @Autowired UserService userService;
+
+  public Mail buildAccVerificationTemplate(long ID) {
+    User user = userService.getById(ID);
+    Mail mail = new Mail();
+
+    Email fromEmail = new Email();
+    fromEmail.setName("Email Authenticator");
+    fromEmail.setEmail("email-authenticator@hours.innexgo.com");
+    mail.setFrom(fromEmail);
+
+    mail.setTemplateId("d-deadbeefdeadbeefdeadbeefdeadbeef"); //TODO set template
+
+    Personalization personalization = new Personalization();
+    personalization.addDynamicTemplateData("name", user.name);
+    //personalization.addDynamicTemplateData("verifLink", ); //TODO add verif link generator 
+    personalization.addTo(new Email(user.email));
+    mail.addPersonalization(personalization);
+
+    return mail;
+  }
+
+  public void dynamicTemplateExample(long ID) throws IOException {
+    final Mail dynamicTemplate = buildAccVerificationTemplate(ID);
+    send(dynamicTemplate);
+  }
+
+  private static void send(final Mail mail) throws IOException {
+    final SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY")); //TODO add .env API KEY
+
+    final Request request = new Request();
+    request.setMethod(Method.POST);
+    request.setEndpoint("mail/send");
+    request.setBody(mail.build());
+
+    final Response response = sg.api(request);
+    //System.out.println(response.getStatusCode());
+    //System.out.println(response.getBody());
+    //System.out.println(response.getHeaders());
+  }
+}
