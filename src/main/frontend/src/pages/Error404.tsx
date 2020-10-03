ea20b1e0cfbd803fa2142eb64d13726f6e0066d9
gpi <gpimpale29@gmail.com>
2020-10-03 12:46:12 -0700

    nice

--- src/main/frontend/src/pages/Error404.tsx
+++ src/main/frontend/src/pages/Error404.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
 
-function Error() {
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Bootstrap CSS & JS
+import '../style/external.scss';
+import 'bootstrap/dist/js/bootstrap';
+import 'popper.js/dist/popper';
+
+function Error404() {
   return (
-    <p> 404 Error: Page does not exist </p>
+    <div style={{
+      display: "flex",
+      height: "100vh",
+      width: "100vw"
+    }}>
+      <div className="my-auto mx-auto text-center">
+        <h1>404 Error.</h1>
+        <h5>Page Not Found</h5>
+        <Link to="/">Return Home</Link>
+      </div>
+    </div>
   )
 }
 
-export default Error;
+export default Error404
