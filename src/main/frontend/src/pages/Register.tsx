7442ae6858fd292c8b6285438731811d83ebf22f
Jordan Nguyen <jonguyen0505@gmail.com>
2020-10-18 16:48:36 -0700

    smol
    - changed attendee to host for student calendar card items (turning teacher calendar card into student)
    - register page , NOT functional , only frontend

--- src/main/frontend/src/pages/Register.tsx
+++ src/main/frontend/src/pages/Register.tsx
@@ -1,30 +1,28 @@
 import React from 'react';
 import { Button, Card, Form } from 'react-bootstrap'
+import { ArrowForward } from '@material-ui/icons';
+import transparent from "../img/innexgo_transparent_icon.png"
 
 import ExternalLayout from '../components/ExternalLayout';
 import { fetchApi } from '../utils/utils';
 
 import innexgo_logo from '../img/innexgo_logo_dark.png';
 
-import blurred_bg from '../img/homepage-bg.png';
-
 
 
 export default function Register() {
-  const bgStyle = {
-    backgroundImage: `radial-gradient(rgba(0, 0, 0, 0.9),rgba(0, 0, 0, 0.1)), url(${blurred_bg})`,
-    height: "100vh",
-    alignItems: "center",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    display: "flex",
-    justifyContent: "center"
-  };
 
   const errorStyle = {
     color: "#DC143C"
   }
+
+  const formBoxStyle = {
+    borderLeft: '0',
+    borderTop: '0',
+    borderRight: '0',
+    borderRadius: '0',
+    borderBottom: '1px solid grey',
+  };
 
   const [errorText, setErrorText] = React.useState("");
   const [userName, setUserName] = React.useState("");
@@ -36,47 +34,42 @@ export default function Register() {
   }
 
   return (
-    <ExternalLayout fixed={false} transparentTop={true}>
-      <div style={bgStyle}>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <h4><img
-                alt="Innexgo Logo"
-                src={innexgo_logo}
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-              />{' '}
-                Innexgo</h4>
-            </Card.Title>
-            <p>Register</p>
-            <Form>
-              <Form.Group>
-                <Form.Control id="username" type="email" placeholder="Email"
-                  onChange={e => {
-                    setUserName(e.target.value);
-                  }} />
-                <br />
-                <Form.Control id="password" type="password" placeholder="Password"
-                  onChange={e => {
-                    setPassword(e.target.value);
-                  }} />
-                  <br />
-                   <Form.Control id="code" type="text" placeholder="District Code"
-                  onChange={e => {
-                    setCode(e.target.value);
-                  }} />
-                <p className="form-text text-danger" id="error"></p>
-              </Form.Group>
-              <Button variant="primary" onClick={async () => postRegister()}>Register</Button>
-              <p style={errorStyle}>{errorText}</p>
-            </Form>
-            <br />
-          </Card.Body>
-        </Card>
+
+    <div style={{ height: '100vh' }}>
+      <div className="row h-100">
+        <div className="h-100 px-5 py-5 w-25 text-light" style={{
+          minWidth: "5rem",
+          backgroundColor: '#990000ff',
+        }}>
+          <img src={transparent} alt="Innexgo Logo" />
+          <h4 className="my-3">Attendance simplified.</h4>
+          <p>To use Innexgo Hours, your school/district has to have a subcription. 
+            To learn more, click <a href="https://hours.innexgo.com" style={{
+              color: 'white',
+              textDecoration: 'underline',
+            }}>here</a>.
+          </p>
+        </div>
+        <div className="h-100 px-5 py-5 w-75" >
+          <Form>
+          <Form.Group>
+            <h4 style={{paddingBottom: '20px'}}>Create an account (teachers)</h4>
+            <Form.Control style={formBoxStyle} id="username" type="email" placeholder="Email"/>
+            <p style={{color:'#484445'}}>Use the email associated with your school/district.</p>
+            
+            <Form.Control style={formBoxStyle} id="password" type="password" placeholder="Password"/>
+            <p style={{color:'#484445'}}>Choose a strong password you'll be able to remember.</p>
+
+            <Form.Control style={formBoxStyle} id="code" type="code" placeholder="District Code"/>
+            <p style={{color:'#484445'}}>This is a code from your district to verify your identity and connect you to your school.</p>
+            <p className="form-text text-danger" id="error"></p>
+          </Form.Group>
+          <Button variant="dark">Register</Button>
+          <p>{/*error stuff*/}</p>
+        </Form>
+        </div>
       </div>
-    </ExternalLayout>
+    </div>
   )
 }
 
