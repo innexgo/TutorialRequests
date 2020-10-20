2f4022a992bcf3cc7c45f92b01163f34c593664b
Jordan Nguyen <jonguyen0505@gmail.com>
2020-10-20 00:33:02 -0700

    removed links in sidebar
    attendance & report begone

--- src/main/frontend/src/components/UserDashboardLayout.tsx
+++ src/main/frontend/src/components/UserDashboardLayout.tsx
@@ -6,8 +6,6 @@ import DashboardLayout from '../components/DashboardLayout';
 export default function(props: React.PropsWithChildren<AuthenticatedComponentProps>) {
   return (<DashboardLayout name={props.apiKey.creator.name} logoutCallback={() => props.setApiKey(null)} >
     <DashboardLayout.SidebarEntry label="Home" icon={Home} href="/user" />
-    <DashboardLayout.SidebarEntry label="Attendance" icon={AccountCircle} href="/attendance" />
-    // <DashboardLayout.SidebarEntry label="Report (TBC)" icon={Assessment} href="/report" />
     <DashboardLayout.Body>
       {props.children}
     </DashboardLayout.Body>
