8c803847f79fe98045fcdbf6963c89becfcda66d
gpi <gpimpale29@gmail.com>
2020-10-23 23:18:36 -0700

    nice

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-java -jar \
-  -Dserver.port=8081 \
-  ./build/libs/school.hours-0.0.1-SNAPSHOT.jar > ~/school.hours.innexgo.com.txt &
+# per deployment settings
+# check out application.properties
+# to see what env vars you need to export
+source settings.sh
+
+java -jar ./build/libs/school.hours-0.0.1-SNAPSHOT.jar > ~/school.hours.innexgo.com.txt &
