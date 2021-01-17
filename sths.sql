5945415fef2bf156486ddcdc0a353bf215581953
gpi <gpimpale29@gmail.com>
2021-01-17 02:59:22 -0800

    added more user data

---
+++ sths.sql
@@ -0,0 +1,496 @@
+INSERT INTO verification_challenge VALUES(
+ '10', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Ben Ninh', -- name
+ 'ben@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 10, -- user_id
+ 1, -- creation_time
+ 'Ben Ninh', --name
+ 'ben@example.com', -- email
+ '10' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 10, -- password_id
+ 1, -- creation_time
+ 10, -- creator_user_id
+ 10, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoYDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+
+
+
+
+INSERT INTO verification_challenge VALUES(
+ '11', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Andrew Muench', -- name
+ 'andrew@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoYDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 11, -- user_id
+ 1, -- creation_time
+ 'Andrew Muench', --name
+ 'andrew@example.com', -- email
+ '11' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 11, -- password_id
+ 1, -- creation_time
+ 11, -- creator_user_id
+ 11, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoYDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+
+
+INSERT INTO verification_challenge VALUES(
+ '12', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Jinsung Park', -- name
+ 'jinsung@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 12, -- user_id
+ 1, -- creation_time
+ '’Jinsung Park', --name
+ 'jinsung@example.com', -- email
+ '12' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 12, -- password_id
+ 1, -- creation_time
+ 12, -- creator_user_id
+ 12, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+INSERT INTO verification_challenge VALUES(
+ '13', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Sean Park', -- name
+ 'sean@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 13, -- user_id
+ 1, -- creation_time
+ 'Sean Park', --name
+ 'sean@example.com', -- email
+ '13' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 13, -- password_id
+ 1, -- creation_time
+ 13, -- creator_user_id
+ 13, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+
+INSERT INTO verification_challenge VALUES(
+ '14', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Ganu Pimaple', -- name
+ 'ganu@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 14, -- user_id
+ 1, -- creation_time
+ 'Ganu Pimpale', --name
+ 'ganu@example.com', -- email
+ '14' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 14, -- password_id
+ 1, -- creation_time
+ 14, -- creator_user_id
+ 14, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+
+INSERT INTO verification_challenge VALUES(
+ '15', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'John Vo', -- name
+ 'john@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 15, -- user_id
+ 1, -- creation_time
+ 'John Vo', --name
+ 'john@example.com', -- email
+ '15' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 15, -- password_id
+ 1, -- creation_time
+ 15, -- creator_user_id
+ 15, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+INSERT INTO verification_challenge VALUES(
+ '16', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Stacy Nguyen', -- name
+ 'stacy@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 16, -- user_id
+ 1, -- creation_time
+ 'Stacy Nguyen', --name
+ 'stacy@example.com', -- email
+ '16' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 16, -- password_id
+ 1, -- creation_time
+ 16, -- creator_user_id
+ 16, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+
+INSERT INTO verification_challenge VALUES(
+ '17', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Viet Ninh', -- name
+ 'viet@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 17, -- user_id
+ 1, -- creation_time
+ 'Viet Ninh', --name
+ 'viet@example.com', -- email
+ '17' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 17, -- password_id
+ 1, -- creation_time
+ 17, -- creator_user_id
+ 17, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+
+
+INSERT INTO verification_challenge VALUES(
+ '18', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Richurtle Le', -- name
+ 'richurtle@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 18, -- user_id
+ 1, -- creation_time
+ 'Richurtle Le', --name
+ 'richurtle@example.com', -- email
+ '18’ -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 18, -- password_id
+ 1, -- creation_time
+ 18, -- creator_user_id
+ 18, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+INSERT INTO verification_challenge VALUES(
+ '19', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Charles Chen', -- name
+ 'charles@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 19, -- user_id
+ 1, -- creation_time
+ 'Charles Chen', --name
+ 'charles@example.com', -- email
+ '19' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 19, -- password_id
+ 1, -- creation_time
+ 19, -- creator_user_id
+ 19, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+
+INSERT INTO verification_challenge VALUES(
+ '20', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Jack Smith', -- name
+ 'jack@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 20, -- user_id
+ 1, -- creation_time
+ 'Jack Smith', --name
+ 'jack@example.com', -- email
+ '20' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 20, -- password_id
+ 1, -- creation_time
+ 20, -- creator_user_id
+ 20, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+
+INSERT INTO verification_challenge VALUES(
+ '21', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Johnson Wong', -- name
+ 'johnson@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 21, -- user_id
+ 1, -- creation_time
+ 'Johnson Wong', --name
+ 'johnson@example.com', -- email
+ '21' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 21, -- password_id
+ 1, -- creation_time
+ 21, -- creator_user_id
+ 21, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+INSERT INTO verification_challenge VALUES(
+ '22', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Luis Bedolla', -- name
+ 'luis@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 22, -- user_id
+ 1, -- creation_time
+ 'Luis Bedolla', --name
+ 'luis@example.com', -- email
+ '22' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 22, -- password_id
+ 1, -- creation_time
+ 22, -- creator_user_id
+ 22, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+INSERT INTO verification_challenge VALUES(
+ '23', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Steven Very', -- name
+ 'steven@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 23, -- user_id
+ 1, -- creation_time
+ 'Steven Very', --name
+ 'steven@example.com', -- email
+ '23' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 23, -- password_id
+ 1, -- creation_time
+ 23, -- creator_user_id
+ 23, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+INSERT INTO verification_challenge VALUES(
+ '24', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Kian Kruk', -- name
+ 'kian@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 24, -- user_id
+ 1, -- creation_time
+ 'Kian Kruk', --name
+ 'kian@example.com', -- email
+ '24' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 24, -- password_id
+ 1, -- creation_time
+ 24, -- creator_user_id
+ 24, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+INSERT INTO verification_challenge VALUES(
+ '25', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Daniel Binoy', -- name
+ 'daniel@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 25, -- user_id
+ 1, -- creation_time
+ 'Daniel Binoy', --name
+ 'daniel@example.com', -- email
+ '25' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 25, -- password_id
+ 1, -- creation_time
+ 25, -- creator_user_id
+ 25, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+INSERT INTO verification_challenge VALUES(
+ '26', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Raycard Quinto’, -- name
+ 'raycard@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 26, -- user_id
+ 1, -- creation_time
+ 'Raycard Quinto', --name
+ 'raycard@example.com', -- email
+ '26' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 26, -- password_id
+ 1, -- creation_time
+ 26, -- creator_user_id
+ 26, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+INSERT INTO verification_challenge VALUES(
+ '27', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Oscar Mayor', -- name
+ 'oscar@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 27, -- user_id
+ 1, -- creation_time
+ 'Oscar Mayor', --name
+ 'oscar@example.com', -- email
+ '27' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 27, -- password_id
+ 1, -- creation_time
+ 27, -- creator_user_id
+ 27, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+INSERT INTO verification_challenge VALUES(
+ '28', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Ronald Mcdonald', -- name
+ 'ronald@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 28, -- user_id
+ 1, -- creation_time
+ 'Ronald Mcdonald', --name
+ 'ronald@example.com', -- email
+ '28' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 28, -- password_id
+ 1, -- creation_time
+ 28, -- creator_user_id
+ 28, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+INSERT INTO verification_challenge VALUES(
+ '29', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Raymond Bottomley', -- name
+ 'raymond@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 29, -- user_id
+ 1, -- creation_time
+ 'Raymond Bottomley', --name
+ 'raymond@example.com', -- email
+ '29' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 29, -- password_id
+ 1, -- creation_time
+ 29, -- creator_user_id
+ 29, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
+INSERT INTO verification_challenge VALUES(
+ '30', -- verification_challenge_key_hash
+ 1, -- creation_time
+ 'Randall Xie', -- name
+ 'randall@example.com', -- email
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.' -- password_hash
+);
+INSERT INTO user VALUES(
+ 30, -- user_id
+ 1, -- creation_time
+ 'Randall Xie', --name
+ 'randall@example.com', -- email
+ '30' -- verification_challenge_key_hash
+);
+INSERT INTO password VALUES(
+ 30, -- password_id
+ 1, -- creation_time
+ 30, -- creator_user_id
+ 30, -- user_id
+ 0, -- password_kind
+ '$2a$10$kteCMggOjaT1lJWybiFwMewtFvec7QB35lo6Rjk7IjNJFVJBoyDQ.', -- password_hash
+ '' -- password_reset_key_hash
+);
